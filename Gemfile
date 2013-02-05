67227c9fc71149d9c9d2450bf7f2c8a52bf189b2
Chris Searle <chris@chrissearle.org>
2013-02-05 14:37:15 +0100

    Initial

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+
+  gem 'font-awesome-sass-rails'
+end
+
+gem 'bootstrap-sass'
+
+gem 'jquery-rails'
+
+gem 'thin'
+
+gem 'simple_form'
+
+gem 'haml'
+
+gem 'foreman', :require => false
+
+group :development do
+	gem 'quiet_assets'
+	gem 'better_errors'
+	gem 'binding_of_caller'
+	gem 'meta_request'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nodalpoint::Application.config.secret_token = '91795b166e873f30adb6c7b4cc8699c71eb9fb79e0df9eb9b77454aa5f6192cbca7f932d03430d3876c77f7c0b15ff3cb3ec7c272d28b0fea5073283cdc943ec'
